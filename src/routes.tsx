c371dc3df0e2cfad2e5b3bc6c0741672994e51be
Wilson <will_workhard@163.com>
2022-09-25 00:22:16 +0800

    feat: 添加路由菜单

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,33 +3,89 @@ import { Login } from '@/pages/Login';
 import { Register } from '@/pages/Register';
 import { Result } from 'antd';
 import { ResetPassword } from './pages/ResetPassword';
-import { Home } from './pages/Home';
 import { BasicLayout } from './layout/BasicLayout';
 
-export interface IMyRouteObject extends RouteObject {
-    key?: string
+interface IMyRouteObject extends RouteObject {
+    key?: string;
     label?: string; // 默认行为，如果不设置label，则不展示在Menu菜单里，
     hideInMenu?: boolean;
     icon?: React.ReactNode;
-    children?: IMyRouteObject[]
+    children?: IMyRouteObject[];
 }
 
-export const routes: IMyRouteObject[] = [
+export type MyRouteObjectWithParent = Omit<IMyRouteObject, 'children'> & {
+    parent: MyRouteObjectWithParent | null;
+    children: MyRouteObjectWithParent[];
+};
+
+const _routes: IMyRouteObject[] = [
     {
         path: '/',
         element: <BasicLayout />,
         children: [
             {
                 label: '系统设置',
+                key: 'system-setting',
                 children: [
-                    { path: 'home', element: <h2>home page </h2> },
-                    { path: 'user', element: <h2>use page </h2> },
+                    { path: 'home', label: '首页', element: <h2>home page </h2> },
+                    { path: 'user', label: '用户中心', element: <h2>use page </h2> },
                 ],
             },
         ],
     },
-    { path: '/login',  element: <Login /> },
+    {
+        path: '/a',
+        element: <BasicLayout />,
+        children: [
+            {
+                label: '系统设置2',
+                key: 'system-setting1',
+                children: [
+                    { path: 'home', label: '首页2', element: <h2>home page </h2> },
+                    { path: 'user', label: '用户中心2', element: <h2>use page </h2> },
+                ],
+            },
+        ],
+    },
+    { path: '/login', hideInMenu: true, element: <Login /> },
     { path: '/register', hideInMenu: true, element: <Register /> },
     { path: '/reset-password', hideInMenu: true, element: <ResetPassword /> },
     { path: '*', hideInMenu: true, element: <Result status='404' title='404' /> },
 ];
+
+function formatPath(path: string) {
+    return path.startsWith('/') ? path : `/${path}`;
+}
+function getRouteKey(route: MyRouteObjectWithParent) {
+    if (!route.path && !route.key) {
+        throw new Error('path和key必须存在其一');
+    }
+
+    if (route.key) {
+        return route.key;
+    }
+
+    let parentPaths: string[] = [];
+    let parent = route.parent;
+    while (parent) {
+        const { path } = parent;
+        path !== undefined && path !== '/' && parentPaths.push(formatPath(path));
+        parent = parent.parent;
+    }
+
+    return parentPaths.reverse().join() + formatPath(route.path!);
+}
+
+function loopRoutes(routes: IMyRouteObject[], parent: MyRouteObjectWithParent | null = null) {
+    routes.forEach((route) => {
+        const _route = route as MyRouteObjectWithParent;
+        _route.parent = parent;
+        _route.key = getRouteKey(_route);
+        if (route.children) {
+            loopRoutes(route.children, _route);
+        }
+    });
+}
+loopRoutes(_routes);
+
+export const routes = _routes as MyRouteObjectWithParent[];
---
+++ todo.md
@@ -0,0 +1,4 @@
+代码实现：
+1. 加工routes, 添加key和parent属性 √
+2. 生成Menu items数据结构
+3. 管理selectedKeys， openKeys状态
